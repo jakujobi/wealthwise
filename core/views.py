ab9e51ddbe1c0b3bbca0f8546c23672172c39862
John <john@jakujobi.com>
2025-02-05 20:16:09 -0600

    Enhance loan calculator UI with Bootstrap styling and create base and home templates

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 # core/views.py
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse("<h1>Welcome to WealthWise!</h1><p>Go to /calculators/loan/ to see the loan calculator.</p>")
+    return render(request, 'home.html')
