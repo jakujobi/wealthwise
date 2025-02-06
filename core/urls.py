c0d8a27355fe25a375948ab12d1b64c9c56a25d6
John <john@jakujobi.com>
2025-02-05 19:40:23 -0600

    Add home view URL routing to core URLs

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# core/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from .views import home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),      # <-- The empty path
     path('admin/', admin.site.urls),
     path('calculators/', include('calculators.urls')),
 ]
