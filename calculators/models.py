db982346148d1db8dbebad069bed05e5246569fb
RedAstro <125700363+trungnguyenn43@users.noreply.github.com>
2025-02-14 00:37:29 -0600

    Add new models for financial tool usage, consultations, events, subscriptions, payments, messaging, and notifications

--- calculators/models.py
+++ calculators/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-
-# Create your models here.
-
+from django.contrib.postgres.fields import JSONField
 
 # budget
+class FinancialToolUsage(models.Model):
+    usage_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(users.Profile, on_delete=models.CASCADE)
+    tool_type = models.CharField(max_length=20)
+    input_data = JSONField()
+    result_data = JSONField()
+    usage_date = models.DateTimeField(auto_now_add=True)
