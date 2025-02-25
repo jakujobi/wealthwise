17f5e22ce411fc61feecec5eda8b64177fb959a6
Sawyer Theis <sawyer.c.theis@gmail.com>
2025-02-25 11:49:57 -0600

    update ProfileForm to include last_name in fields

--- users/forms.py
+++ users/forms.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_picture', 'phone_number', 'address', 'city', 'state', 'country', 'postal_code']
+        fields = ['profile_picture', 'last_name', 'phone_number', 'phone_number', 'address', 'city', 'state', 'country', 'postal_code']
